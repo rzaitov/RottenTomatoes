4fae2ad4ecf737d367df9c62a7a5d331de7a7e14
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2014-04-27 17:40:37 +0400

    start implement movie details screen

---
+++ RottenTomatoes/Screens/MovieDetails/MovieDetailsView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using MonoTouch.UIKit;
+
+using Logic;
+
+namespace RottenTomatoes
+{
+	public class MovieDetailsView : UIView
+	{
+		public MovieDetailsView()
+		{
+		}
+
+		public void BindMovieDetails(Movie movie)
+		{
+
+		}
+	}
+}
+
---
+++ RottenTomatoes/Screens/MovieDetails/MovieDetailsViewController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using MonoTouch.UIKit;
+
+namespace RottenTomatoes
+{
+	public class MovieDetailsViewController : UIViewController
+	{
+		public MovieDetailsViewController()
+		{
+		}
+	}
+}
+
